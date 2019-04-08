ccd223a87f3d40a737e7190bd987574e35ee7903
Ashish Tyagi <ashishtyagii@outlook.com>
2019-04-08 04:23:55 -0700

    C++ operations

---
+++ implicit_assignment/implicit_assignment.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float width = 4.5;
+    float height = 5.5;
+
+    int area = width * height;
+    std::cout << "area = " << area<<endl;
+          return 0;
+     }
---
+++ implicit_assignment/implicit_assignment2.cpp
@@ -0,0 +1,13 @@
+ #include <iostream>
+
+     int main(void)
+     {
+         int numerator = 4;
+         float denominator = 5;
+         float answer=0;
+         
+
+         answer = numerator / denominator;
+         std::cout<<"answer = "<<answer;
+         return 0;
+     }
